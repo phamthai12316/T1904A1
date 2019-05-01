e9cee14bf0f92cf78b7a99aff19a22e530e1a223
phamthai12316 <49876579+phamthai12316@users.noreply.github.com>
2019-05-01 20:52:28 +0700

    BTVN1

---
+++ AmDuong.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Nhap vao so a: ");
+	scanf("%d",&a);
+	if(a>0)
+	{
+    	printf ("a la so duong! \n");
+	}
+	else
+	{
+    	printf ("a la so am! \n");	
+	}
+}
